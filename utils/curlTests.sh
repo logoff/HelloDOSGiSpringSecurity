572bdb28e5a2c0d5b94c487e0b5ddf21eb67f5d6
logoff <juliocarlos.barrera@i2cat.net>
2013-04-23 13:10:28 +0200

    Updated Curls tests script

--- utils/curlTests.sh
+++ utils/curlTests.sh
@@ -5,42 +5,73 @@ echo "| Hello DOSGi Spring Security Tests |"
 echo "|                                   |"
 echo "|-----------------------------------|"
 echo ""
+#=========== Response codes ================#
 res200="HTTP/1.1 200"
 res401="HTTP/1.1 401"
 res403="HTTP/1.1 403"
-echo "user:user (403 Forbidden expected):"
+#===========     sayHello   ================#
+message="sayHello - user:user        (403 Forbidden expected):   "
 result=$(curl -s -i -H "Authorization: Basic dXNlcjp1c2Vy" -X GET http://localhost:8181/sample/sayHello?name=user)
 if [[ "$result" = $res403* ]]
 then
-	echo "OK"
+	echo "$message - OK"
 else
-	echo "Error!Actual response: ${result:0:12}"
+	echo "$message - Error! Actual response: ${result:0:12}"
 fi
-echo ""
-echo "admin:admin (200 OK expected):"
+message="sayHello - admin:admin      (200 OK expected):          "
 result=$(curl -s -i -H "Authorization: Basic YWRtaW46YWRtaW4=" -X GET http://localhost:8181/sample/sayHello?name=user)
 if [[ "$result" = $res200* ]]
 then
-	echo "OK"
+	echo "$message - OK"
 else
-	echo "Error! Actual response: ${result:0:12}"
+	echo "$message - Error! Actual response: ${result:0:12}"
 fi
-echo ""
-echo "abc:abc (401 Unauthorized expected):"
+message="sayHello - abc:abc          (401 Unauthorized expected):"
 result=$(curl -s -i -H "Authorization: Basic YWJjOmFiYw==" -X GET http://localhost:8181/sample/sayHello?name=user)
 if [[ "$result" = $res401* ]]
 then
-	echo "OK"
+	echo "$message - OK"
 else
-	echo "Error! Actual response: ${result:0:12}"
+	echo "$message - Error! Actual response: ${result:0:12}"
 fi
-echo ""
-echo "<No auth header> (401 Unauthorized expected):"
+message="sayHello - <No auth header> (401 Unauthorized expected):"
 result=$(curl -s -i -X GET http://localhost:8181/sample/sayHello?name=user)
 if [[ "$result" = $res401* ]]
 then
-	echo "OK"
+	echo "$message - OK"
 else
-	echo "Error! Actual response: ${result:0:12}"
+	echo "$message - Error! Actual response: ${result:0:12}"
 fi
-
+#===========     sayBye   ================#
+message="sayBye   - user:user        (200 OK expected):          "
+result=$(curl -s -i -H "Authorization: Basic dXNlcjp1c2Vy" -X GET http://localhost:8181/sample/sayBye?name=user)
+if [[ "$result" = $res200* ]]
+then
+	echo "$message - OK"
+else
+	echo "$message - Error! Actual response: ${result:0:12}"
+fi
+message="sayBye   - admin:admin      (200 OK expected):          "
+result=$(curl -s -i -H "Authorization: Basic YWRtaW46YWRtaW4=" -X GET http://localhost:8181/sample/sayBye?name=user)
+if [[ "$result" = $res200* ]]
+then
+	echo "$message - OK"
+else
+	echo "$message - Error! Actual response: ${result:0:12}"
+fi
+message="sayBye   - abc:abc          (401 Unauthorized expected):"
+result=$(curl -s -i -H "Authorization: Basic YWJjOmFiYw==" -X GET http://localhost:8181/sample/sayBye?name=user)
+if [[ "$result" = $res401* ]]
+then
+	echo "$message - OK"
+else
+	echo "$message - Error! Actual response: ${result:0:12}"
+fi
+message="sayBye   - <No auth header> (401 Unauthorized expected):"
+result=$(curl -s -i -X GET http://localhost:8181/sample/sayBye?name=user)
+if [[ "$result" = $res401* ]]
+then
+	echo "$message - OK"
+else
+	echo "$message - Error! Actual response: ${result:0:12}"
+fi
